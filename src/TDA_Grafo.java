ba6f562d61b54d865ec2392cc775badaecc3c716
Ramiro Zayas <ramirozayas134@gmail.com>
2024-10-28 14:52:30 -0300

    Una interface de TDA Grafos pára utilizarla en la clase grafos

---
+++ src/TDA_Grafo.java
@@ -0,0 +1,9 @@
+import java.util.List;
+public interface TDA_Grafo {
+    void agregarVertice(int vertice); // Agrega un vértice al grafo
+    void eliminarVertice(int vertice); // Elimina un vértice del grafo
+    void agregarArista(int origen, int destino, int peso); // Agrega una arista dirigida entre dos vértices con un peso
+    void eliminarArista(int origen, int destino); // Elimina una arista dirigida entre dos vértices
+    boolean GrafoVacio();
+    List<Integer> obtenerAdyacentes(int vertice); // Devuelve una lista de adyacencia o una lista de vértices adyacentes
+}
