98dcaefa91ce1301bd7a6810bd78e5c7274e4a39
Ramiro Zayas <ramirozayas134@gmail.com>
2024-10-28 17:34:17 -0300

    Clase de grafo con la implementacion de TDA_Grafo, falta desarrollar

---
+++ src/Grafo.java
@@ -0,0 +1,21 @@
+import java.util.List;
+public class Grafo implements TDA_Grafo{
+    public void agregarVertice(int vertice) {
+
+    }
+    public void eliminarVertice(int vertice) {
+
+    }
+    public void agregarArista(int origen, int destino, int peso) {
+
+    }
+    public void eliminarArista(int origen, int destino) {
+
+    }
+    public boolean GrafoVacio() {
+        return false;
+    }
+    public List<Integer> obtenerAdyacentes(int vertice) {
+        return null;
+    }
+}
