2e46d11b7fa967ac60240465e6ed2748e9296ded
Ramiro Zayas <ramirozayas134@gmail.com>
2024-11-02 12:04:24 -0300

    Clase Arista, Nodo y modificacion e implementacion en la Clase Grafo

---
+++ src/Arista.java
@@ -0,0 +1,8 @@
+public class Arista {
+    Nodo destino;
+    int peso;
+    public Arista(Nodo destino, int peso) {
+        this.destino = destino;
+        this.peso = peso;
+    }
+}
---
+++ src/Nodo.java
@@ -0,0 +1,9 @@
+public class Nodo {
+    private int id;  // ID para identificar cada nodo(Ya sea escuela o gimnasio)
+    public Nodo(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+}
